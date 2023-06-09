a89810bbcc6b7ae4767fb25950a8fcebac7eb047
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-06-09 18:02:24 +0100

    Committing Extension Folder in WebApi Directory to local repository

---
+++ InventorySalesDemo.WebApi/ServiceExtensions/ExceptionMiddleWareExtension.cs
@@ -0,0 +1,13 @@
+﻿using InventorySalesDemo.Application.Common;
+
+namespace InventorySalesDemo.WebApi.ServiceExtensions
+{
+    public static class ExceptionMiddleWareExtension
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app, ILoggerManager logger)
+        {
+            app.UseExceptionHandler(appError => { });
+        }
+    }
+}
+
---
+++ InventorySalesDemo.WebApi/ServiceExtensions/ServiceExtension.cs
@@ -0,0 +1,13 @@
+﻿using InventorySalesDemo.Application.Common;
+using System.Runtime.CompilerServices;
+
+namespace InventorySalesDemo.WebApi.ServiceExtensions
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigureRepositoryManager(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, IRepositoryManager>();
+        }
+    }
+}
