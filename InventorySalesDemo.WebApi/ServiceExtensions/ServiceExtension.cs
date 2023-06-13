bc67f9cfff8c97bd42a617a67e7e3fadb5a53299
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-06-13 09:51:33 +0100

    Committing Service Extension file and others to local repository

--- InventorySalesDemo.WebApi/ServiceExtensions/ServiceExtension.cs
+++ InventorySalesDemo.WebApi/ServiceExtensions/ServiceExtension.cs
@@ -1,13 +1,38 @@
 ﻿using InventorySalesDemo.Application.Common;
+using InventorySalesDemo.Persistence.Common;
+using LoggerService;
 using System.Runtime.CompilerServices;
 
 namespace InventorySalesDemo.WebApi.ServiceExtensions
 {
     public static class ServiceExtension
     {
+       
+        public static void ConfigureCors(this IServiceCollection services) 
+        {
+            services.AddCors(option =>
+            {
+                option.AddPolicy("CorsPolicy", builder => builder
+                .AllowAnyHeader()
+                .AllowAnyOrigin()
+                .AllowAnyMethod());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(option => { });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+
         public static void ConfigureRepositoryManager(this IServiceCollection services)
         {
-            services.AddScoped<IRepositoryManager, IRepositoryManager>();
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
         }
     }
+
 }
